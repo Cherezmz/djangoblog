5ee0ccdddfad6253dd6768885f30a97f54e9d663
Igor Slabykh <igor.slabykh@gmail.com>
2020-01-29 00:08:08 -0500

    add templates directory into settings to use templates

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,12 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def about(request):
-    return HttpResponse("about")
+    # return HttpResponse("about")
+    return render(request, 'about.html')
 
 
 def home(request):
-    return HttpResponse("homepage")
+    # return HttpResponse("homepage")
+    return render(request, 'homepage.html')
