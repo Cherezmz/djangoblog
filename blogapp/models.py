0c48d0b07a418583f727277d43d5c103094d5b1f
Igor Slabykh <igor.slabykh@gmail.com>
2020-01-29 14:09:30 -0500

    created models

--- blogapp/models.py
+++ blogapp/models.py
@@ -1,3 +1,41 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Article (models.Model):
+    # STATUS_CHOICES = (
+    #     ('draft', 'Draft'),
+    #     ('published', 'Published'),
+    # )
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(null=True)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    author = models.ForeignKey(
+        User, default=None, on_delete=models.CASCADE, related_name='articles')
+
+# tag = models.CharField(max_length=100)
+# link = models.CharField(max_length=100)
+# code_snippet = models.FilePathField(path="/img")
+# slug = models.SlugField(null=True, unique_for_date='publish')
+# publish = models.DateTimeField()
+# created = models.DateTimeField(auto_now_add=True)
+# updated = models.DateTimeField(auto_now=True)
+# status = models.CharField(
+#     max_length=10, choices=STATUS_CHOICES, default='draft')
+
+
+# to sort published articles in descending order
+# class Meta:
+#     ordering = ('-publish',)
+
+
+# def __str__(self):
+#     return self.title
+
+
+class Comment (models.Model):
+    article = models.ForeignKey(Article, on_delete=models.CASCADE)
+    author = models.CharField(max_length=50)
+    body = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
