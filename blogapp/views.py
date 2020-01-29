113337940896c498cc52ae20588e7b6a959c58f0
Igor Slabykh <igor.slabykh@gmail.com>
2020-01-29 16:50:32 -0500

    tries to add render. I had pylint error: for Article.object.all() no objects. Fixed this changing VS code settings but now cannot open browser(attributr error: no objects)

--- blogapp/views.py
+++ blogapp/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
+from .models import Article, Comment
 
 # Create your views here.
 
 
 def articles_list(request):
+    # articles = Article.object.all().order_by('date')
     return render(request, 'blogapp/articles_list.html')
