8a50feae99aa0a7b5ceae03382bf85055c4a7faa
Igor Slabykh <igor.slabykh@gmail.com>
2020-01-29 22:23:07 -0500

    attempt to create a new user using form shows error (Reverse for ' list' not found. ' list' is not a valid view function or pattern name.) that os connected with line 12 of accounts/views. But a user has been added

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
 
+app_name = "articles"
+
 urlpatterns = [
-    path('', views.articles_list, name='articles_list'),
+    path('', views.articles_list, name='list'),
 ]
